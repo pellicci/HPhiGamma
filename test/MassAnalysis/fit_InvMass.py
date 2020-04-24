b912e7385eaabe54e9cfe7a8f922b48b1fed445d
Giulio Umoret <gumoret@lxplus747.cern.ch>
2020-04-24 09:28:55 +0200

    fit updated

--- test/MassAnalysis/fit_InvMass.py
+++ test/MassAnalysis/fit_InvMass.py
@@ -1,32 +1,36 @@
 import ROOT
 
+#INPUT SETTINGS
+fInput = ROOT.TFile("workspaces/ws_dataCombined.root")
+fInput.cd()
+workspace = fInput.Get("myworkspace")
+workspace.Print()
 
-mass = ROOT.RooRealVar("_HiggsMass","_HiggsMass",100.,150.,"GeV/c^2")
-mass.setRange("LowSideband",100.,120.)
-mass.setRange("HighSideband",130.,150.)
-
+#VAR SETTINGS
+mass_KKg = workspace.var("mass_KKg")
 a_bkg = ROOT.RooRealVar("a_bkg","a_bkg",0.1,-10.,10.)
 b_bkg = ROOT.RooRealVar("b_bkg","b_bkg",0.,-10.,10.)
 c_bkg = ROOT.RooRealVar("c_bkg","c_bkg",0.,-10.,10.)
 d_bkg = ROOT.RooRealVar("d_bkg","d_bkg",0.,-10.,10.)
 e_bkg = ROOT.RooRealVar("e_bkg","e_bkg",0.,-10.,10.)
 
-bkgPDF = ROOT.RooChebychev("bkgPDF","bkgPDF",mass,ROOT.RooArgList(a_bkg,b_bkg,c_bkg,d_bkg,e_bkg))
+#PDF
+bkgPDF = ROOT.RooChebychev("bkgPDF","bkgPDF",mass_KKg,ROOT.RooArgList(a_bkg,b_bkg,c_bkg,d_bkg,e_bkg))
+print "bkgPDF created!"
 
-fileInput = ROOT.TFile("histos/latest_production/histos_Data.root")
-fileInput.cd()
-tree = fileInput.Get("tree_output")
+#DATASET
+dataCombined = workspace.data("totPDFData")
+print "dataCombined retrieved!"
 
-dataset = ROOT.RooDataSet("dataset","dataset",ROOT.RooArgSet(mass),ROOT.RooFit.Import(tree))
+#FIT
+bkgPDF.fitTo(dataCombined)
+print "dataCombined fitted!"
 
-bkgPDF.fitTo(dataset,ROOT.RooFit.Range("LowSideband,HighSideband"))
-
-data_blinded = dataset.reduce("_HiggsMass < 120. || _HiggsMass > 130.")
-xframe = mass.frame(30)
-data_blinded.plotOn(xframe)
-bkgPDF.plotOn(xframe,ROOT.RooFit.Range("LowSideband,HighSideband"))
+xframe = mass_KKg.frame(40)
+dataCombined.plotOn(xframe)
+bkgPDF.plotOn(xframe)
 
 c1 = ROOT.TCanvas()
 c1.cd()
 xframe.Draw()
-c1.SaveAs("fitdata.pdf")
+c1.SaveAs("fit_dataCombined.pdf")
