33e95035835e89c9c7091e7ef6bd9a74e3e45897
Giulio Umoret <gumoret@lxplus747.cern.ch>
2020-04-24 09:31:17 +0200

    fit updated

---
+++ test/MassAnalysis/create_combinedDataset.py
@@ -0,0 +1,74 @@
+import ROOT
+import argparse
+import os
+
+debug = True
+
+p = argparse.ArgumentParser(description='Select directory')
+p.add_argument('dir_path', help='Type directory path')
+args = p.parse_args()
+
+#INPUT SETTINGS-------------------------------------------------------------
+fInput1 = ROOT.TFile("workspaces/ws_signal.root")
+fInput2 = ROOT.TFile("workspaces/ws_data.root")
+fInput3 = ROOT.TFile(args.dir_path + "histos_Signal.root")
+
+#TREE RETRIEVING------------------------------------------------------------
+Signal_tree = fInput3.Get("tree_output")
+Signal_entries = Signal_tree.GetEntriesFast()
+
+#GET WORKSPACES
+fInput1.cd()
+workspaceSignal = fInput1.Get("myworkspace")
+fInput2.cd()
+workspaceData = fInput2.Get("myworkspace")
+
+#workspaceSignal.Print()
+#workspaceData.Print()
+
+#RETRIEVE PDFs
+sigPDF = workspaceSignal.pdf("signalPDF")
+backgroundPDF = workspaceData.pdf("bkgPDF")
+
+#N. OF EVENTS
+signal_amplifier = 1.
+signalWeight = 0.692480078053*signal_amplifier
+Ndata = 789
+Nsig = ROOT.RooRealVar("Signal_events","N of signal events",signalWeight,signalWeight-0.5*signalWeight,signalWeight+0.5*signalWeight) 
+Nbkg = ROOT.RooRealVar("Bkg events", "N of bkg events",Ndata,Ndata-0.5*Ndata,Ndata+0.5*Ndata)
+
+#ARGLISTs and VARs
+argListPDFs = ROOT.RooArgList(sigPDF,backgroundPDF)
+argListN = ROOT.RooArgList(Nsig,Nbkg)
+mass_KKg = ROOT.RooRealVar("mass_KKg","The invariant mass_KKg",125.,100.,150.,"GeV/c^2")
+
+#ADD PDFs
+totPDF = ROOT.RooAddPdf("totPDF","The total PDF",argListPDFs,argListN)
+print "PDFs added!"
+
+#GENERATE DATA
+dataCombined = totPDF.generate(ROOT.RooArgSet(mass_KKg),Ndata)
+
+#PLOT
+mass_KKgplot = mass_KKg.frame(40)
+print "mass_KKg plot created!"
+dataCombined.plotOn(mass_KKgplot)
+print "dataCombined plotted!"
+totPDF.plotOn(mass_KKgplot)
+print "totpdf plotted!"
+
+canvas = ROOT.TCanvas()
+canvas.cd()
+mass_KKgplot.Draw()
+canvas.SaveAs("PDFtot.pdf")
+
+#SAVE DATACOMBINED
+workspace = ROOT.RooWorkspace("myworkspace")
+getattr(workspace,'import')(dataCombined)
+
+fOut = ROOT.TFile("workspaces/ws_dataCombined.root","RECREATE")
+fOut.cd()
+workspace.Write()
+fOut.Close()
+
+del workspace
---
+++ test/MassAnalysis/fit_Data.py
@@ -0,0 +1,42 @@
+import ROOT
+
+mass = ROOT.RooRealVar("mass_KKg","mass_KKg",100.,150.,"GeV/c^2")
+mass.setRange("LowSideband",100.,120.)
+mass.setRange("HighSideband",130.,150.)
+
+a_bkg = ROOT.RooRealVar("a_bkg","a_bkg",0.1,-10.,10.)
+b_bkg = ROOT.RooRealVar("b_bkg","b_bkg",0.,-10.,10.)
+c_bkg = ROOT.RooRealVar("c_bkg","c_bkg",0.,-10.,10.)
+d_bkg = ROOT.RooRealVar("d_bkg","d_bkg",0.,-10.,10.)
+e_bkg = ROOT.RooRealVar("e_bkg","e_bkg",0.,-10.,10.)
+
+bkgPDF = ROOT.RooChebychev("bkgPDF","bkgPDF",mass,ROOT.RooArgList(a_bkg,b_bkg,c_bkg,d_bkg,e_bkg))
+
+fileInput = ROOT.TFile("histos/latest_production/histos_Data.root")
+fileInput.cd()
+tree = fileInput.Get("tree_output")
+
+dataset = ROOT.RooDataSet("dataset","dataset",ROOT.RooArgSet(mass),ROOT.RooFit.Import(tree))
+nEntries = dataset.numEntries() 
+
+bkgPDF.fitTo(dataset,ROOT.RooFit.Range("LowSideband,HighSideband"))
+
+data_blinded = dataset.reduce("mass_KKg < 120. || mass_KKg > 130.")
+xframe = mass.frame(30)
+data_blinded.plotOn(xframe)
+bkgPDF.plotOn(xframe,ROOT.RooFit.Range("LowSideband,HighSideband"))
+
+c1 = ROOT.TCanvas()
+c1.cd()
+xframe.Draw()
+c1.SaveAs("fitdata.pdf")
+
+#create Workspace
+print "**************************************n. events = ",nEntries
+workspace = ROOT.RooWorkspace("myworkspace")
+getattr(workspace,'import')(bkgPDF)
+
+fOut = ROOT.TFile("workspaces/ws_data.root","RECREATE")
+fOut.cd()
+workspace.Write()
+fOut.Close()
