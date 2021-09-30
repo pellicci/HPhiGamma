a840a65c7f0f6007480349340110d75de8570bc1
Mario <mpel@protonmail.com>
2021-09-30 12:20:39 +0200

    adding haddnano

--- generation/testScript_queue.sh
+++ generation/testScript_queue.sh
@@ -2,9 +2,9 @@
 
 HOMEDIR=/afs/cern.ch/user/p/pellicci/work/HPhiGamma/Prod/CMSSW_10_6_17_patch1/src/HiggsAnalysis/HPhiGamma/generation
 CMSSW_TO_USE=CMSSW_10_6_17_patch1
-INPUTDIR=/eos/user/p/pellicci/MesonGamma_root/2018/HRhoGamma_VBF/
-OUTPUTDIR=/eos/user/p/pellicci/MesonGamma_root/2018/HRhoGamma_VBF/GEN
-PYTHONAME=HMesonGamma_MINI_2018_cfg.py
+INPUTDIR=/eos/user/p/pellicci/MesonGamma_root/2018/HRhoGamma_VBF/HLT
+OUTPUTDIR=/eos/user/p/pellicci/MesonGamma_root/2018/HRhoGamma_VBF/RECO
+PYTHONAME=HMesonGamma_RECO_2018_cfg.py
 
 #this is necessary only if EOS access is required
 export X509_USER_PROXY=/afs/cern.ch/user/p/pellicci/voms_proxy/x509up_u28550
@@ -34,11 +34,11 @@ scram b
 
 jobNumber=$(($1+$OFFSET));
 
-#cat << EOF >> $PYTHONAME
-#from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper
-#randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)
-#randSvc.populate()
-#EOF
+cat << EOF >> $PYTHONAME
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)
+randSvc.populate()
+EOF
 
 xrdcp $INPUTDIR/process_${jobNumber}.root process_IN.root
 
