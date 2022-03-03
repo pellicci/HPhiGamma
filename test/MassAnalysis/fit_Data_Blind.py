d4de512cb9d87e847e44ab188e79c9ab6bd842b1
Giulio Umoret <giulio.umoret@cern.ch>
2022-03-03 08:44:20 +0100

    Mass analysis 1.1

--- test/MassAnalysis/fit_Data_Blind.py
+++ test/MassAnalysis/fit_Data_Blind.py
@@ -1,89 +1,133 @@
 import ROOT
 
-#Supress the opening of many Canvas's
-ROOT.gROOT.SetBatch(True) 
+#Some useful things before starting --------------------------------------------------------------------
+ROOT.gROOT.SetBatch(True) #Supress the opening of many Canvas's
+ROOT.gROOT.ProcessLineSync(".L MassAnalysis/dCB/RooDoubleCBFast.cc+") #import the Doube Crystal Ball PDF
 
-#INPUT from workspaces
-fInput1 = ROOT.TFile("workspaces/ws_signal.root")
-fInput1.cd()
-workspaceSignal = fInput1.Get("myworkspace")
 
-fInput2 = ROOT.TFile("workspaces/ws_sidebands.root")
-fInput2.cd()
-workspaceSidebands = fInput2.Get("myworkspace")
+#Define the observable and sidebands --------------------------
+mass = ROOT.RooRealVar("mass_KKg","mass_KKg",100.,150.,"GeV")
+mass.setRange("LowSideband",100.,120.)
+mass.setRange("HighSideband",130.,150.)
 
-#fInput3 = ROOT.TFile("nEvents_Container.root")
-#fInput3.cd()
-#ytree = fInput3.Get("tree_output")
-#mytree.Scan()
 
-fInput4 = ROOT.TFile("histos/latest_production/histos_SR_Data.root")
-fInput4.cd()
-tree = fInput4.Get("tree_output")
+#Data input -------------------------------------------------------------
+fileInputData = ROOT.TFile("histos/latest_production/histos_SR_Data.root")
+fileInputData.cd()
+mytree = fileInputData.Get("tree_output")
 
-#VARS and CONSTANTS
-workspaceSignal.var("mass_KKg").setConstant(1)
-workspaceSignal.var("mean").setConstant(1)
-workspaceSignal.var("alpha").setConstant(1)
-workspaceSignal.var("sigma").setConstant(1)
-workspaceSignal.var("enne").setConstant(1)
+
+#Import dataset ---------------------------------------------------------------------------
+data = ROOT.RooDataSet("dataset","dataset",ROOT.RooArgSet(mass),ROOT.RooFit.Import(mytree))
+print "Using ", data.numEntries(), " events to fit"
+
+
+#Import from workspaces --------------------------------------------------------------------
+fInputSignalPDF = ROOT.TFile("workspaces/ws_signal.root")
+fInputSignalPDF.cd()
+workspaceSignal = fInputSignalPDF.Get("myworkspace")
+
+#Signal PDF
+workspaceSignal.var("dCB_aL").setConstant(1)
+workspaceSignal.var("dCB_nL").setConstant(1)
+workspaceSignal.var("dCB_aR").setConstant(1)
+workspaceSignal.var("dCB_nR").setConstant(1)
+workspaceSignal.var("dCB_pole").setConstant(1)
+workspaceSignal.var("dCB_width").setConstant(1)		
+sigPDF = workspaceSignal.pdf("signalPDF") #SIGNAL PDF
+
+#Background PDF
+fInputSidebandsPDF = ROOT.TFile("workspaces/ws_sidebands.root")
+fInputSidebandsPDF.cd()
+workspaceSidebands = fInputSidebandsPDF.Get("myworkspace")
+bkgPDF = workspaceSidebands.pdf("bkgPDF") #BKG PDF
 
 #Number of events
-Ndata = 3617 #number of events in the sidebands of m_KKg #mytree.nEvents_BKG
-signalWeight = 0.227113240117 #mytree.nEvents_Signal
-print "Ndata = ", Ndata
-print "Nsig = ", signalWeight
+#Ndata = 3617 #number of events in the sidebands of m_KKg
+#signalWeight = 0.227113240117 #sum of signal weights event by event
+#print "Ndata = ", Ndata
+#print "Nsig = ", signalWeight
 
-Nbkg = ROOT.RooRealVar("Nbkg", "N of bkg events",Ndata, 0.1, 5000)
-mass = ROOT.RooRealVar("mass_KKg","mass_KKg",100.,150.,"GeV")
 
-#BKG PDF
-bkgPDF = workspaceSidebands.pdf("bkgPDF")
+#Define the POI and an offset for blind analysis -------------------------------------------------------------------------------------
+B_R = ROOT.RooRealVar("B_R","branching_ratio",10**(-5),-1,10**(-2)) #parameter of interest
+B_R_blind = ROOT.RooUnblindOffset("B_R_blind","B_R_blind","aSeedString",0.000001,B_R) #generate an unkown offset to be applied to the blind window
 
-#RIPARAMETRIZATION
+#Define PDFs for the fit --------------------------------------------------------------------------------------------------
 cross_sig = ROOT.RooRealVar("cross_sig","Th e signal cross section",52.36)#52.36pb (gluon fusion + vector boson fusion xsec)
 lumi = ROOT.RooRealVar("lumi","The luminosity",39540)#pb^-1
 eff = ROOT.RooRealVar("eff","efficiency",0.058251) # = 2898/49750 = 0.058251
 B_R_phiKK = ROOT.RooRealVar("B_R_phiKK","b_r_phi_KK",0.489) # = BR(phi -> K+K-)
-B_R_ = ROOT.RooRealVar("B_R_","branching_ratio",10**(-5),-1,10**(-2))
-Nsig = ROOT.RooFormulaVar("Nsig","@0*@1*@2*@3*@4",ROOT.RooArgList(cross_sig,lumi,eff,B_R_phiKK,B_R_))
-
-#SIGNAL PDF
-sigPDF = workspaceSignal.pdf("signalPDF")
-
-#Retrieve dataset from the tree, insert the variable also
-dataset = ROOT.RooDataSet("dataset","dataset",ROOT.RooArgSet(mass),ROOT.RooFit.Import(tree))
-nEntries = dataset.numEntries() 
+Nsig = ROOT.RooFormulaVar("Nsig","@0*@1*@2*@3*@4",ROOT.RooArgList(cross_sig,lumi,eff,B_R_phiKK,B_R)) #for Signal
+Nbkg = ROOT.RooRealVar("Nbkg", "N of bkg events",700, 300, 5000) #for BKG
 
 #ADD PDFs
 argListPDFs = ROOT.RooArgList(sigPDF,bkgPDF)
 argListN = ROOT.RooArgList(Nsig,Nbkg)
 totPDF = ROOT.RooAddPdf("totPDF","The total PDF",argListPDFs,argListN)
-print "pdf added"
+print "PDFs added!"
 
-#FIT
-totPDF.fitTo(dataset)
+#Do the fit --------------------------------------------------------------------------------------------------------------
+result_dataFit = totPDF.fitTo(data,ROOT.RooFit.Extended(1), ROOT.RooFit.NumCPU(4), ROOT.RooFit.Save() ) #For the signal region, I want the fit to be extended (Poisson fluctuation of number of events) 
+																										#to take into account that the total number of events is the sum of signal and background events. 
+																										#Either I do this, or I use a fraction frac*Nbkg+(1-frac)*Nsig, which will become a parameter of 
+																										#the fit and will have a Gaussian behavior (whilst the extended fit preserves the natural Poisson behavior)
 
-#PLOT
-xframe = mass.frame(50)
-xframe.SetTitle("#sqrt{s} = 13 TeV       lumi = 39.54/fb")
-xframe.GetXaxis().SetTitle("m_{K^{+}K^{-}#gamma} [GeV]")
-dataset.plotOn(xframe)
+#Plots -------------------------------------------------------------------------------------------------------------------
+xframe = mass.frame(55.,95.,15)
+xframe.SetTitle(" ")
+xframe.SetTitleOffset(1.4,"y")
+xframe.SetMaximum(90)
+
+#Exclude the control regions
+data_reduced = dataset.reduce("mass_KKg < 120. || mass_KKg > 130.")
+data_reduced.plotOn(xframe)
 totPDF.plotOn(xframe)
 
-c1 = ROOT.TCanvas()
-c1.cd()
-c1.SetTitle("")
+#Draw on canvas
+canvas = ROOT.TCanvas()
+canvas.cd()
 xframe.Draw()
-c1.SaveAs("~/cernbox/www/MyAnalysis/HPhiGamma/MassAnalysis/latest_production/fitdataBlind.pdf")
-c1.SaveAs("~/cernbox/www/MyAnalysis/HPhiGamma/MassAnalysis/latest_production/fitdataBlind.png")
+
+#Save the plot
+canvas.SaveAs("~/cernbox/www/MyAnalysis/HPhiGamma/MassAnalysis/latest_production/fitdataBlind.pdf")
+canvas.SaveAs("~/cernbox/www/MyAnalysis/HPhiGamma/MassAnalysis/latest_production/fitdataBlind.png")
+
+#Write to file -------------------------------------------------------------------------------------------------------------------
+fOutput = ROOT.TFile("workspaces/ws_data_blind.root","RECREATE")
+fOutput.cd()
+
+workspace_out = ROOT.RooWorkspace("workspace")
+getattr(workspace_out,'import')(data)
+getattr(workspace_out,'import')(totPDF)
+
+workspace_out.Write()
+
+fOutput.Close()
+del workspace_out
+
+raw_input()
+
+#PLOT
+#xframe = mass.frame(50)
+#xframe.SetTitle("#sqrt{s} = 13 TeV       lumi = 39.54/fb")
+#xframe.GetXaxis().SetTitle("m_{K^{+}K^{-}#gamma} [GeV]")
+#dataset.plotOn(xframe)
+#totPDF.plotOn(xframe)
+
+#c1 = ROOT.TCanvas()
+#c1.cd()
+#c1.SetTitle("")
+#xframe.Draw()
+#c1.SaveAs("~/cernbox/www/MyAnalysis/HPhiGamma/MassAnalysis/latest_production/fitdataBlind.pdf")
+#c1.SaveAs("~/cernbox/www/MyAnalysis/HPhiGamma/MassAnalysis/latest_production/fitdataBlind.png")
 
 #create Workspace
-print "**************************************n. events = ",nEntries
-workspace = ROOT.RooWorkspace("myworkspace")
-getattr(workspace,'import')(totPDF)
+#print "**************************************n. events = ",nEntries
+#workspace = ROOT.RooWorkspace("myworkspace")
+#getattr(workspace,'import')(totPDF)
 
-fOut = ROOT.TFile("workspaces/ws_data_blind.root","RECREATE")
-fOut.cd()
-workspace.Write()
-fOut.Close()
+#fOut = ROOT.TFile("workspaces/ws_data_blind.root","RECREATE")
+#fOut.cd()
+#workspace.Write()
+#fOut.Close()
--- test/MassAnalysis/fit_Signal.py
+++ test/MassAnalysis/fit_Signal.py
@@ -1,18 +1,33 @@
 import ROOT
 
+#import the Doube Crystal Ball PDF
+ROOT.gROOT.ProcessLineSync(".L MassAnalysis/dCB/RooDoubleCBFast.cc+")
+
 #Supress the opening of many Canvas's
 ROOT.gROOT.SetBatch(True)   
 
 #Parameters of the PDF
-mass = ROOT.RooRealVar("mass_KKg","mass_KKg",125.,100.,150.,"GeV/c^2") #var name has to be the same of there's in the tree
-mean = ROOT.RooRealVar("mean","mean",125.,100.,150.)
-width = ROOT.RooRealVar("width","width",2.5,0.1,5.)
-sigma = ROOT.RooRealVar("sigma","sigma",2.,0.1,5.)
-alpha = ROOT.RooRealVar("alpha","alpha",1.,0.1,10.)
-enne = ROOT.RooRealVar("enne","enne",2.,0.1,20.)
+#mass = ROOT.RooRealVar("mass_KKg","mass_KKg",125.,100.,150.,"GeV/c^2") #var name has to be the same of there's in the tree
+#mean = ROOT.RooRealVar("mean","mean",125.,100.,150.)
+#width = ROOT.RooRealVar("width","width",2.5,0.1,5.)
+#sigma = ROOT.RooRealVar("sigma","sigma",2.,0.1,5.)
+##alpha = ROOT.RooRealVar("alpha","alpha",1.,0.1,10.)
+#enne = ROOT.RooRealVar("enne","enne",2.,0.1,20.)
+
+#Define the observable
+mass = ROOT.RooRealVar("mass_KKg","mass_KKg",125.,100.,150.,"GeV/c^2")
+
+#Double Crystal Ball definition
+dCB_pole  = ROOT.RooRealVar("dCB_pole", "Double CB pole", 125.,100.,150.)
+dCB_width = ROOT.RooRealVar("dCB_width", "Double CB width",1.,0.01,10.)
+dCB_aL    = ROOT.RooRealVar("dCB_aL", "Double CB alpha left", 3., 0.1, 50.)
+dCB_aR    = ROOT.RooRealVar("dCB_aR", "Double CB alpha right", 1., 0.1, 50.)
+dCB_nL    = ROOT.RooRealVar("dCB_nL", "Double CB n left", 3., 0.1, 50.)
+dCB_nR    = ROOT.RooRealVar("dCB_nR", "Double CB n right", 1., 0.1, 50.)
+signalPDF = ROOT.RooDoubleCBFast("dCB", "Double Crystal Ball", mass, dCB_pole, dCB_width, dCB_aL, dCB_nL, dCB_aR, dCB_nR)
 
 #Initialize a Crystal Ball pdf
-signalPDF = ROOT.RooCBShape("signalPDF","signalPDF",mass,mean,sigma,alpha,enne)
+#signalPDF = ROOT.RooCBShape("signalPDF","signalPDF",mass,mean,sigma,alpha,enne)
 
 #Input file and tree
 fileInput = ROOT.TFile("histos/latest_production/histos_SR_Signal.root")
