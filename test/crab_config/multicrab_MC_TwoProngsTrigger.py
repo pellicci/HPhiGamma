8d2f04ae89b0c3cd6303ab65a736b9016e54090c
gumoret <giulio.umoret@cern.ch>
2022-07-05 09:27:40 +0200

    multicrab trigger add

---
+++ test/crab_config/multicrab_MC_TwoProngsTrigger.py
@@ -0,0 +1,63 @@
+from CRABAPI.RawCommand import crabCommand
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config
+config = Configuration()
+
+config.section_('General')
+config.General.transferOutputs = True
+config.General.workArea = 'crab_projects/samples_MC'
+
+config.section_('JobType')
+config.JobType.psetName = 'cmssw_config/run_HPhiGammaTwoProngsTriggerAnalysis.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.inputFiles = ['MCpileUp_2018_25ns_UltraLegacy_PoissonOOTPU.root','MyDataPileupHistogram.root' ] #data files for PileUp reweighting
+config.JobType.outputFiles = ['HPhiGammaTwoProngsTriggerAnalysis_output.root']
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pyCfgParams = ['runningOnData=False'] # Configure 2016 data jobs - muons
+
+config.section_('Data')
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.publication = False
+
+config.section_('Site')
+config.Site.storageSite = 'T2_IT_Legnaro'
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    from multiprocessing import Process
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+
+    #################################################
+    #                                               #
+    #--------------- Running 2018 MC ---------------#
+    #                                               #
+    #################################################
+
+config.General.requestName = 'HPhiGammaTwoProngsTriggerAnalysis_DY10to50'
+config.Data.unitsPerJob = 5
+config.Data.inputDataset = '/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v2/MINIAODSIM'
+p = Process(target=submit, args=(config,))
+p.start()
+p.join()
+
+config.General.requestName = 'HPhiGammaTwoProngsTriggerAnalysis_DY50' #Requires FileBased splitting
+config.Data.unitsPerJob = 5
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v1/MINIAODSIM'
+p = Process(target=submit, args=(config,))
+p.start()
+p.join()
